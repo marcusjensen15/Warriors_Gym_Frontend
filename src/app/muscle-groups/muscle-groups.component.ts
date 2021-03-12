32bd7c3881234fc57792eabdd5f84968826aab80
Marcus Jensen <marcusjensen1515@gmail.com>
2021-03-11 20:16:37 -0800

    add heading to /musclegroups

--- src/app/muscle-groups/muscle-groups.component.ts
+++ src/app/muscle-groups/muscle-groups.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-muscle-groups',
   templateUrl: './muscle-groups.component.html',
-  styleUrls: ['./muscle-groups.component.sass']
+  styleUrls: ['./muscle-groups.component.scss']
 })
 export class MuscleGroupsComponent implements OnInit {
 
