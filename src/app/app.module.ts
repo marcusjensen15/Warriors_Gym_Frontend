fc35e3c87d8aebff2f70983d6a93b0a691e2cf50
Marcus Jensen <marcusjensen1515@gmail.com>
2021-03-22 17:56:12 -0700

    import radio buttons. They still need formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//All views requiring routes are encapsulated within 'routingComponents'
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular Material Imports:
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 import { HeaderComponent } from "./header/header.component";
@@ -28,7 +31,8 @@ import { HeaderComponent } from "./header/header.component";
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
