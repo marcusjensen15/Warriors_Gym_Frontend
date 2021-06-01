4ccc4b182285b615aa5c7c353a6bb9e458d9ade4
Marcus Jensen <marcusjensen1515@gmail.com>
2021-05-31 19:31:13 -0700

    remove unnecessary content type in api call

--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -11,12 +11,11 @@ export class GetApiService {
   ) { }
 
   // Adjust link below to real get users API call. Need to figure out how this works with tokens.
-    // Will need to create an auth service, pass in token as argument in future. For now, just hard code it to get this wired up at a basic level
+  // Will need to create an auth service, pass in token as argument in future. For now, just hard code it to get this wired up at a basic level
 
   getUsers(){
     return this.http.get<any>('http://localhost:3000/users', {
       headers: {
-        'Content-Type' : "application/x-www-form-urlencoded; charset=UTF-8",
         "x-auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI1NGVmYWEzM2ViODA0MjhiZDkyYjkiLCJpc0FkbWluIjp0cnVlLCJpc01hbmFnZXIiOnRydWUsImlhdCI6MTYyMjQ5NTA3Nn0.TkP4OBL401w22wNnmqaGdEO9Nfw10A-xiiDKkRfWIbo"}
     });
   };
