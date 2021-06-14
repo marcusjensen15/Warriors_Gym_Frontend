71b699c0423076b91d1410a7db2d4b594c4e8088
Marcus Jensen <marcusjensen1515@gmail.com>
2021-06-13 17:34:43 -0700

    sucessfully showing error message in view, need to get it to conditionally render users vs error message using ng if

--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -25,19 +25,6 @@ export class GetApiService {
   })
       .pipe(
         catchError(this.errorHandler));
-
-
-    // return response
-
-    // return this.http
-    //   .get("http://localhost:3000/users")
-    //   .subscribe(
-    //     data => console.log('success', data),
-    //     error => console.log('oops', error.error)
-    //   );
-    // .catch((err: HttpErrorResponse) => {
-    //   console.log("error has occured", err.error)
-    // });
   };
 
   errorHandler(error: HttpErrorResponse){
