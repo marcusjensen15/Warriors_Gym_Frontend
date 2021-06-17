220543a5683c8ad7f2ebad5114a3a65b95404ebe
Marcus Jensen <marcusjensen1515@gmail.com>
2021-06-16 18:10:35 -0700

    home component tests passing

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -22,4 +22,12 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show login button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="loginButton"]')).toBeTruthy();
+    });
+
+  it('should show signup button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="signupButton"]')).toBeTruthy();
+  });
 });
