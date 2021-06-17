1ca5d62dbf6fed90d3b92e6c3f592826580435c7
Marcus Jensen <marcusjensen1515@gmail.com>
2021-06-16 18:50:23 -0700

    all signup tests passing

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,25 +1,36 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { LoginComponent } from './login.component';
-//
-// describe('LoginComponent', () => {
-//   let component: LoginComponent;
-//   let fixture: ComponentFixture<LoginComponent>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ LoginComponent ]
-//     })
-//     .compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LoginComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show email address field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="loginEmailField"]')).toBeTruthy();
+  });
+
+  it('should show password field', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="loginPasswordField"]')).toBeTruthy();
+  });
+});
