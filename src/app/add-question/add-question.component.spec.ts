65706c502243e0c5932e7b2c73a3820d8553b1d0
Marcus Jensen <marcusjensen1515@gmail.com>
2021-06-16 19:49:44 -0700

    add question tests passing

--- src/app/add-question/add-question.component.spec.ts
+++ src/app/add-question/add-question.component.spec.ts
@@ -23,7 +23,37 @@ describe('AddQuestionComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show header logo', () => {
-    expect(fixture.nativeElement.querySelector('[data-test="headerLogo"]')).toBeTruthy();
+  it('should show add question title', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="questionTitle"]')).toBeTruthy();
   });
+
+    it('should show muscle group input', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="muscleGroupInput"]')).toBeTruthy();
+  });
+
+    it('should show question text input', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="questionTextInput"]')).toBeTruthy();
+  });
+
+    it('should show question 1 input', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="questionOneInput"]')).toBeTruthy();
+  });
+
+    it('should show question 2 input', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="questionTwoInput"]')).toBeTruthy();
+  });
+
+    it('should show question 3 input', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="questionThreeInput"]')).toBeTruthy();
+  });
+
+    it('should show question 4 input', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="questionFourInput"]')).toBeTruthy();
+  });
+
+    it('should show add question button', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="addQuestionButton"]')).toBeTruthy();
+  });
+
+
 });
