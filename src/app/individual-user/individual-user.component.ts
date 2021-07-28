194e0515ec7df873168e9d37ad8f50442c5e2bea
Marcus Jensen <marcusjensen1515@gmail.com>
2021-07-27 17:58:30 -0700

    individual user route works

---
+++ src/app/individual-user/individual-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-individual-user',
+  templateUrl: './individual-user.component.html',
+  styleUrls: ['./individual-user.component.sass']
+})
+export class IndividualUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
