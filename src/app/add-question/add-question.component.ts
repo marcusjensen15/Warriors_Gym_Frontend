bf456edc9ba52fb60d935174bda610970b29298b
Marcus Jensen <marcusjensen1515@gmail.com>
2021-08-17 07:42:35 -0700

    looping through muscle groups instead of hard coding them

--- src/app/add-question/add-question.component.ts
+++ src/app/add-question/add-question.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddQuestionComponent implements OnInit {
 
+  public muscleGroups = ["Tournaments", "Assessments", "Training", "Resources", "Courses", "Administration", "Content", "Metrics"];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onQuestionFormSubmit(data){
+    console.log('button pressed');
   }
 
 }
