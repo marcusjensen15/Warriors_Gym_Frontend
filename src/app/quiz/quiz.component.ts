78c9dc1cba02cf98cbe1a4870ede24539390a3c0
Marcus Jensen <marcusjensen1515@gmail.com>
2021-08-29 12:15:43 -0700

    correctly serving first question from backend

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -12,25 +12,97 @@ export class QuizComponent implements OnInit {
   public quizQuestions = [] as any;
   public quizCategory : string;
 
+  //will keep track of the total number of questions the user has completed
+
+  public quizProgressCounter = 0;
+
+  //This variable will hold the current question object
+
+  public currentQuestion = <any> {};
+
+  //will count the number of answers the user has gotten correct:
+  public correctAnswerCounter : number;
+
+  //will get the correct answer position of the current question
+  public correctAnswerArrayPosition : number;
+
+  //will hold the user's submitted answer
+
+  public userSubmittedAnswer : number;
+
+  //false if there are additional questions, true if not.
+  // in HTML file, will render 'results' instead of another question if 'true'
+
+  public isFinalQuestion : boolean;
+
   constructor(private activatedRoute: ActivatedRoute, private api:GetApiService) { }
 
   getQuestionsByCategoryHandler(questionCategory) {
-    return this.api.getQuestionsByCategory(questionCategory).subscribe(
+   return this.api.getQuestionsByCategory(questionCategory).subscribe(
       data => {
         data.forEach((element:any) => {this.quizQuestions.push(element);
         });
         console.log(this.quizQuestions);
+        this.setQuestion();
       }
     );
   }
+
+  getSubmittedAnswerArrayPosition(){
+    //will collect the answer submitted by the user, and set the userSubmittedAnser
+  }
+
+  setQuestion() {
+    this.currentQuestion = this.quizQuestions[this.quizProgressCounter];
+    console.log(this.currentQuestion);
+    //will set the current question
+  };
+
+  adjustQuizProgress(){
+    //will count the 'question number' that the user is on
+    //adjust 'quizProgressCounter' accordingly
+  }
+
+  getCorrectAnswerArrayPosition(){
+    //Get the correct answer position of the current question via API.
+  }
+
+  adjustScore(){
+    //will compare userSubmittedAnswer v correctAnswerArrayPosition, and increment
+    //correctAnswerCounter if they are the same
+  };
+
+  serveNextQuestion(){
+    //will serve the next question after the user submits an answer
+    //will also check to see if the current question is the last question
+  };
+
+  isFinalQuestionCheck(){
+    //will check to see if this is the final question in the quiz, if so
+    //on submit turn 'isFinalQuestion' to 'true'
+    //will conditionally render 'results' instead of another question
+  };
+
+  onSubmitAnswer(){
+    //will call all applicable functions:
+    // getCorrectAnswerArrayPosition ->
+    // getSubmittedAnswerArrayPosition ->
+    // adjustScore ->
+    // adjustQuizProgress ->
+    // serveNextQuestion ->
+    // isFinalQuestionCheck ->
+
+  };
 
   getQuizCateogry(){
     this.quizCategory = this.activatedRoute.snapshot.params['quizCategory'];
   }
 
-  ngOnInit() {
-    this.getQuizCateogry();
-    this.getQuestionsByCategoryHandler(this.quizCategory);
+  async ngOnInit() {
+     await this.getQuizCateogry();
+     await this.getQuestionsByCategoryHandler(this.quizCategory);
+     // setTimeout( this.setQuestion(this.quizCategory), 2000);
+     // await this.setQuestion(this.quizCategory);
   }
 
 }
